854fa3aa32ef6030ca7edcf331ff2282b8216461
Kevin Isom <kevin.isom@gmail.com>
2018-12-02 17:42:57 +1300

    Tidy up from testing

--- templates/full/src/server/models/user.js
+++ templates/full/src/server/models/user.js
@@ -1,10 +1,6 @@
 const bookshelf = require('../bookshelf')
-require('./candidate')
 
 module.exports = bookshelf.model('User', {
   tableName: 'users',
-  idAttribute: 'id',
-  votes: function() {
-    return this.belongsToMany('Candidate', 'votes', 'user_id', 'candidate_id')
-  },
+  idAttribute: 'id'
 })
