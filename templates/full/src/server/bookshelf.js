d80c082ddeff5ea4625bc2a9981012dbfeaca851
Kevin Isom <kevin.isom@gmail.com>
2019-10-25 07:37:58 +1300

    Update for bookshelf update

--- templates/full/src/server/bookshelf.js
+++ templates/full/src/server/bookshelf.js
@@ -5,7 +5,6 @@ const knex = Knex(knexfile[process.env.NODE_ENV || 'development'])
 
 const bookshelf = Bookshelf(knex)
 
-bookshelf.plugin('registry')
-bookshelf.plugin('virtuals')
+bookshelf.plugin('bookshelf-virtuals-plugin')
 
 module.exports = bookshelf
