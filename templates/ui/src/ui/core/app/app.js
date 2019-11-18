c01497c9386aa420ac2059d11127cfa5ae2d0cee
Kevin Isom <kevin.isom@gmail.com>
2019-11-19 11:24:01 +1300

    fix issues

--- templates/ui/src/ui/core/app/app.js
+++ templates/ui/src/ui/core/app/app.js
@@ -1,14 +1,14 @@
 import React, { Fragment } from 'react'
 import { Router } from '@reach/router'
 import { NavBar, NavBarBrand, Section } from '@brightleaf/elements'
-import { Loading } from 'components/loading'
-import { AuthProvider } from 'core/context/auth'
-import { AppProvider } from 'core/context/app'
+import { Loading } from 'ui/components/loading'
+import { AuthProvider } from 'ui/core/context/auth'
+import { AppProvider } from 'ui/core/context/app'
 import './app.scss'
-const About = React.lazy(() => import('features/about'))
-const Home = React.lazy(() => import('features/home'))
-const Contact = React.lazy(() => import('features/contact'))
-const Login = React.lazy(() => import('features/login'))
+const About = React.lazy(() => import('ui/features/about'))
+const Home = React.lazy(() => import('ui/features/home'))
+const Contact = React.lazy(() => import('ui/features/contact'))
+
 export default () => {
   return (
     <AuthProvider>
@@ -28,7 +28,6 @@ export default () => {
               <Home path="/" />
               <About path="/about" />
               <Contact path="/contact" />
-              <Login path="/login" />
             </Router>
           </React.Suspense>
         </Section>
